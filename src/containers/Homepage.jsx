fd49485ec5aa63ce40bfea8ca9b5fa0f65781f9b
Jonah Min <jonahm2472@gmail.com>
2025-10-28 22:19:23 -0400

    Add life list page

--- src/containers/Homepage.jsx
+++ src/containers/Homepage.jsx
@@ -12,13 +12,23 @@ export default class HomePage extends PureComponent {
                     <img src="/me.jpg" />
                     <span className="about">
                         <h3>A Little Bit About Me</h3>
-                        <p>I graduated from Northeastern University in 2019 with a major in Computer Science and a minor in Art, and I've been a senior software engineer at HubSpot ever since. I love bird watching, and in my free time I enjoy drawing, playing Magic: the Gathering with friends, running, biking, and hiking.</p>
+                        <p>I graduated from Northeastern University in 2019 with a major in Computer Science and a minor in Art, and I've been a senior software engineer at HubSpot ever since. I love bird watching, drawing, playing Magic: the Gathering with friends, running, biking, and hiking.</p>
                     </span>
                     <span className="projects">
                         <h3>Personal Projects</h3>
                         <ul className="projects-list">
-                            <li><NavLink to="/lifelist">Life list</NavLink>: A list of every bird species I've ever seen.</li>
-                            <li><NavLink to="/emojify">Emojify</NavLink>: A (stupid) website I made to generate messages out of emojis at work.</li>
+                            <li>
+                                <span className="project-link">
+                                    <NavLink to="/lifelist">Life list</NavLink> -
+                                    <p>A list of every bird species I've ever seen.</p>
+                                </span>
+                            </li>
+                            <li>
+                                <span className="project-link">
+                                    <NavLink to="/emojify">Emojify</NavLink> -
+                                    <p>A website I made to generate messages out of emojis at work.</p>
+                                </span>
+                            </li>
                         </ul>
                     </span>
                 </span>
