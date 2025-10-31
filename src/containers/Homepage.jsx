d75f8cdfa25c3363e2ce3190f299a493353529fa
Jonah Min <jonahm2472@gmail.com>
2025-10-31 11:20:24 -0400

    Mobile responsive

--- src/containers/Homepage.jsx
+++ src/containers/Homepage.jsx
@@ -12,14 +12,14 @@ export default class HomePage extends PureComponent {
                     <img src="/me.jpg" />
                     <span className="about">
                         <h3>A Little Bit About Me</h3>
-                        <p>I graduated from Northeastern University in 2019 with a major in Computer Science and a minor in Art, and I've been a senior software engineer at HubSpot ever since. I love bird watching, drawing, playing Magic: the Gathering with friends, running, biking, and hiking.</p>
+                        <p>I graduated from Northeastern University in 2019 with a major in Computer Science and a minor in Art, and I've been a senior software engineer at HubSpot ever since. I enjoy bird watching, drawing, playing Magic: the Gathering, running, biking, and hiking.</p>
                     </span>
                     <span className="projects">
                         <h3>Personal Projects</h3>
                         <ul className="projects-list">
                             <li>
                                 <span className="project-link">
-                                    <NavLink to="https://lifelist.jonahmin.com">Bird Watching Life list</NavLink> -
+                                    <NavLink to="https://lifelist.jonahmin.com">Life list</NavLink> -
                                     <p>A list of every single bird I've ever seen.</p>
                                 </span>
                             </li>
