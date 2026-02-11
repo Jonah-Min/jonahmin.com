73234f94f3d3f6a82b6c3f9e3993db74bcf26ce6
Jonah Min <jonahm2472@gmail.com>
2026-02-10 20:41:15 -0500

    Add rare birds map project

--- src/containers/Homepage.jsx
+++ src/containers/Homepage.jsx
@@ -22,31 +22,33 @@ export default class HomePage extends PureComponent {
                     </div>
                     <div className="projects">
                         <h3>Personal Projects</h3>
-                        <ul className="projects-list">
-                            <li>
-                                <div className="project-link">
-                                    <NavLink to="https://lifelist.jonahmin.com">Life list</NavLink> -
-                                    <p>
-                                        In bird watching, a life list catalogues every single bird species you've ever seen, including when and where you saw it.
-                                        A popular software for keeping track of your list is eBird, but personally I prefer to just to keep track of it myself in a spreadsheet.
-                                        I spun up this simple website to display my list alongside the photos (some good, some bad) I've taken of some of the birds over the years.
-                                        It's also got a map of all the places I've gone bird watching. I'm hoping to continue to add features (and birds) to it!
-                                    </p>
-                                </div>
-                            </li>
-                            <li>
-                                <div className="project-link">
-                                    <NavLink to="https://emojify.jonahmin.com">Emojify</NavLink> -
-                                    <p>
-                                        During our onboarding at HubSpot, a friend and I decided to use some of the down time to spin up a website so you could easily generate messages in Slack only using emojis.
-                                        We manually went through every custom emoji at the time to catalogue the ones that vaguely looked like letters in the alphabet.
-                                        I'm now hosting this website so coworkers have access to the tool too!
-                                    </p>
-                                </div>
-                            </li>
-                        </ul>
+                        <div className="project-link">
+                            <NavLink to="https://lifelist.jonahmin.com">Life list</NavLink>
+                            <p>
+                                In bird watching, a life list catalogues every single bird species you've ever seen, including when and where you saw it.
+                                A popular software for keeping track of your list is eBird, but personally I prefer to just to keep track of it myself in a spreadsheet.
+                                I spun up this simple website to display my list alongside the photos (some good, some bad) I've taken of some of the birds over the years.
+                                It's also got a map of all the places I've gone bird watching. I'm hoping to continue to add features (and birds) to it!
+                            </p>
+                        </div>
+                        <div className="project-link">
+                            <NavLink to="https://emojify.jonahmin.com">Emojify</NavLink>
+                            <p>
+                                During our onboarding at HubSpot, a friend and I decided to use some of the down time to spin up a website so you could easily generate messages in Slack only using emojis.
+                                We manually went through every custom emoji at the time to catalogue the ones that vaguely looked like letters in the alphabet.
+                                I'm now hosting this website so coworkers have access to the tool too!
+                            </p>
+                        </div>
+                        <div className="project-link">
+                            <NavLink to="https://rarebirds.jonahmin.com">Rare birds map</NavLink>
+                            <p>
+                                The popular bird watching website eBird has an API that lets you see nearby rare birds based on a given latitude
+                                and longitude. I decided to use that API to create a little app that includes pins where there are notable sightings.
+                                You can input any latitude and longitude you like to see rare birds in the area!
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </div >
             </div >
         );
     }
